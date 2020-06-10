cee0d8e7b450fa69fb0e1744ed06d2bc4bf012c7
201830671297 <201830671297@users.noreply.github.com>
2020-06-10 20:41:27 +0800

    实现报名功能，报名前检测是否已经报名和attendNUm有没有超，报名后增加

---
+++ sqlwork/src/main/java/com/cn/zqlnb/sql/sqlwork/dao/takeDao.java
@@ -0,0 +1,11 @@
+package com.cn.zqlnb.sql.sqlwork.dao;
+
+import com.cn.zqlnb.sql.sqlwork.pojo.take;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface takeDao {
+    public List<Integer> getTakesByOpenId(String openid);
+    public int addTakes(@Param("Take") take Take);
+}
--- sqlwork/src/main/java/com/cn/zqlnb/sql/sqlwork/dao/turnDao.java
+++ sqlwork/src/main/java/com/cn/zqlnb/sql/sqlwork/dao/turnDao.java
@@ -2,9 +2,12 @@ package com.cn.zqlnb.sql.sqlwork.dao;
 
 import com.cn.zqlnb.sql.sqlwork.pojo.activity;
 import com.cn.zqlnb.sql.sqlwork.pojo.turn;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface turnDao {
     public List<turn> getTurnByPlaceName(String placeName);
+    public turn getTurnByTurnId(Integer turn_id);
+    public int updateTurn(@Param("Turn")turn Turn);
 }
