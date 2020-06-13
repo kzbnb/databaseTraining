0330ef2475697149ee5e475f1f3b7d006dce916b
201830671297 <201830671297@users.noreply.github.com>
2020-06-14 00:21:17 +0800

    鄢志豪已经跳楼了，要我帮他传一下他祖传的bug

--- sqlwork/src/main/java/com/cn/zqlnb/sql/sqlwork/dao/turnDao.java
+++ sqlwork/src/main/java/com/cn/zqlnb/sql/sqlwork/dao/turnDao.java
@@ -3,6 +3,8 @@ package com.cn.zqlnb.sql.sqlwork.dao;
 import com.cn.zqlnb.sql.sqlwork.pojo.activity;
 import com.cn.zqlnb.sql.sqlwork.pojo.turn;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -11,4 +13,5 @@ public interface turnDao {
     public turn getTurnByTurnId(Integer turn_id);
     public int updateTurn(@Param("Turn")turn Turn);
     public int addTurn(turn turn );
+    public List<turn> getTurnByTurnIdArray(@Param("turn_id") List<Integer> turn_id);
 }
